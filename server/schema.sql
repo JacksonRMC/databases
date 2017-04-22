c3b77499dd6c118a7479fd0f06fd1c931a013e04
Hack Reactor Student <student@hackreactor.com>
2017-04-21 17:30:31 -0700

    preliminary schema for chatterbox data

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,92 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+
 
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` MEDIUMTEXT NOT NULL,
+  `current_room` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` MEDIUMTEXT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE messages ( 
+  'id' INTEGER NOT NULL AUTO_INCREMENT DEFAULT 0, 
+  'message' MEDIUMTEXT,
+  'in_room' INTEGER NOT NULL, 
+  'user' INTEGER NOT NULL,
+  PRIMARY KEY ( ' id ' ) 
+);
+
+-- ---
+-- Table 'room_messages'
+-- 
+-- ---
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `users` ADD FOREIGN KEY (current_room) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (user) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `room_messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `user_messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`name`,`current_room`) VALUES
+-- ('','','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`message`,`room`,`user`) VALUES
+-- ('','','','');
+-- INSERT INTO `room_messages` (`id`) VALUES
+-- ('');
+-- INSERT INTO `user_messages` (`id`) VALUES
+-- ('');
